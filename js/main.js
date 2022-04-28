8d804be8378d94162236f1408432a48ab0da611e
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-04-28 17:05:57 +0200

    impostazione richiesta

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// 1. Chiedi numero di chilometri
+// 2. Chiedi età passeggero
+// 3. Definisci prezzo biglietto secondo i km percorsi (0.21€ al km)
+// 4. Inserisci sconto per 20% persone con età < a 18
+// 5. Inserisci sconto del 40% per persone con età > a 65
+// 6. visualizza prezzo finale biglietto con cifra decimale non oltre 2 posizioni
